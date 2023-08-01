e743ae95a42701c54fe465be99e6b7d30dab53de
Vishalecs <95082470+Vishalecs@users.noreply.github.com>
2023-08-01 22:20:53 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -13,7 +13,34 @@ const userSchema =  new mongoose.Schema({
     },
     name :{
         type: String,
+        required: trueconst mongoose = require('mongoose')
+
+const userSchema =  new mongoose.Schema({
+    email :{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password :{
+        type: String,
         required: true
+        
+    },
+    name :{
+        type: String,
+        required: true
+      
+    }
+},
+     
+
+     {
+        timestamps:true
+    });
+    
+  
+    const User = mongoose.model('User', userSchema);
+    module.exports = User;
       
     }
 },
