441f63e44700abf6cad2ddff9f48e1967f83ecc2
Vishalecs <95082470+Vishalecs@users.noreply.github.com>
2023-08-01 22:23:04 +0530

    Update interview.js

--- routes/interview.js
+++ routes/interview.js
@@ -5,9 +5,7 @@ const interviewController = require('../controller/interviewController');
 
 router.get('/add', interviewController.renderAddInterviewForm);
 router.post('/add', interviewController.addInterview);
-router.get('/edit/:id', interviewController.renderEditInterviewForm);
-router.post('/edit/:id', interviewController.updateInterview);
-router.delete('/delete/:id', interviewController.deleteInterview);
 router.get('/interviews', interviewController.displayInterviews);
+
 
 module.exports = router;
